376a5957ccc9fe76e0e1794130efbf927fbee491
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-03-06 19:50:47 -0800

    added suggested services

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -12,15 +12,15 @@ const suggestedService = [
   },
   {
     imageUrl: "/assets/icons/roadtest.svg",
-    title: "Driving Lessons",
+    title: "Road Test",
     description:
-      "All lessons are 90 minutes long and includes pick up and drop off. Refer to our packages for additional details.",
+      "Our Road Test Package includes 45 minutes warm up at either Mckenzie or Duncin ICBC. At a cost of $175. Students are responsible for all ICBC charges.",
   },
   {
     imageUrl: "/assets/icons/targetedpractice.svg",
-    title: "Driving Lessons",
+    title: "Targeted Practices",
     description:
-      "All lessons are 90 minutes long and includes pick up and drop off. Refer to our packages for additional details.",
+      "In addition to all the packages, we understand that each student is different. Some may just want to polish on one or two practices. Either on Parallel Parking or Stall reverse parking. We designed this lesson for such students at a cost of $75. We are willing to work with you.",
   },
 ];
 
@@ -40,39 +40,43 @@ const Hero = () => {
         </div>
         <Container>
           <div className="flex h-full flex-col justify-center gap-y-5 pt-[25vh] lg:gap-y-8">
-            <h2 className="relative text-2xl font-semibold tracking-wider text-primary after:absolute after:left-[-220px] after:top-[50%] after:hidden after:h-[4px] after:w-[200px] after:rounded-full after:bg-primary lg:ml-10 lg:text-3xl after:lg:block">
+            <h2 className="relative text-lg font-semibold tracking-wider text-primary after:absolute after:left-[-220px] after:top-[50%] after:hidden after:h-[4px] after:w-[200px] after:rounded-full after:bg-primary sm:text-2xl lg:ml-10 lg:text-3xl after:lg:block">
               Experience The Art of Driving
             </h2>
-            <h1 className="text-6xl font-extrabold tracking-wider text-white lg:text-7xl">
+            <h1 className="text-4xl font-extrabold tracking-wider text-white sm:text-6xl lg:text-7xl">
               4 Wheelz
               <br />
               Driving School
             </h1>
-            <p className="text-xl font-light text-gray-300 lg:text-2xl">
+            <p className="font-light text-gray-300 sm:text-xl lg:text-2xl">
               Victoria, British Columbia
             </p>
-            <div className="mt-5 flex gap-5">
-              <a className="cursor-pointer rounded-md border-2 border-primary bg-primary px-10 py-4 font-semibold text-white transition-all hover:-translate-y-2 hover:scale-105 md:text-lg">
+            <div className="mt-5 flex flex-col gap-5 sm:flex-row">
+              <a className="cursor-pointer self-start rounded-md border-2 border-primary bg-primary px-10 py-4 font-semibold text-white transition-all hover:-translate-y-2 hover:scale-105 md:text-lg">
                 Book Now
               </a>
-              <a className="cursor-pointer rounded-md border-2 border-primary px-10 py-4 font-semibold text-primary transition-all hover:-translate-y-2 hover:scale-105 md:text-lg">
+              <a className="cursor-pointer self-start rounded-md border-2 border-primary px-10 py-4 font-semibold text-primary transition-all hover:-translate-y-2 hover:scale-105 md:text-lg">
                 Learn More
               </a>
             </div>
           </div>
 
-          <div className=" mt-[10vh] grid w-full grid-cols-3 gap-6">
+          <div className="mt-[10vh] grid w-full gap-6 align-middle sm:px-10 md:grid-cols-2 lg:grid-cols-3">
             {suggestedService.map((item) => (
-              <div className="rounded-md bg-white p-9">
+              <div className="rounded-md bg-white p-9 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.05),_10px_10px_30px_4px_rgba(255,122,0,.05)]">
                 <Image
                   src={item.imageUrl}
                   alt="driving lessons"
                   width={200}
                   height={200}
-                  className="w-32"
+                  className="w-28 lg:w-32 "
                 ></Image>
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
+                <h1 className="mt-5 text-xl font-bold text-gray-800 after:bottom-0 after:my-4 after:block after:h-[3px] after:w-20 after:rounded-xl after:bg-primary lg:text-2xl">
+                  {item.title}
+                </h1>
+                <p className="shortener mt-7 text-sm font-light text-gray-500 lg:text-base">
+                  {item.description}
+                </p>
               </div>
             ))}
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mb-[1000px]">
       <Hero />
     </div>
   );
