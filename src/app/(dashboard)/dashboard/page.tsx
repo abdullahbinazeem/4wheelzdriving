221d598fd0891f1536349cbcecddf251f040bc68
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-04-05 00:57:58 -0700

    fixed db

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -23,7 +23,7 @@ const page = async () => {
 
   return (
     <Container className="grainy min-h-screen pt-20">
-      <h1 className="text-2xl font-bold">Dashboard -</h1>
+      <h1 className="text-2xl font-bold">Dashboard</h1>
       <div className="mt-2 flex gap-6 text-blue-400 underline">
         <p className="cursor-pointer">View Calendar</p>
         <OpeningTimes scheduledDays={scheduleDays} />
